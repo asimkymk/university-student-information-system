37de8ed809e759665aa2811088281520c89a4fa3
emncnozge <emincanozge@outlook.com>
2022-04-20 02:14:10 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -14,24 +14,24 @@ function App() {
             <form>
               <div className="input-group mb-3 mt-5">
                 <div id="tcp" className="input-group-prepend">
-                  <span className="input-group-text h-100" id="basic-addon1"><i className="fa fa-id-badge "></i></span>
+                  <span className="input-group-text h-100" id="basic-addon1" style={{background:"#D5AA41", borderColor:"#D5AA41"}}><i className="fa fa-id-badge "></i></span>
                 </div>
-                <input type="text" maxLength="11" id="tc" className="form-control" placeholder="T.C. Kimlik Numarası" aria-describedby="basic-addon1" required 
-                onInvalid={e => e.target.setCustomValidity('Lütfen T.C. Kimlik Numaranızı doldurunuz.')} onInput={e => e.target.setCustomValidity('')}/>
+                <input type="text" maxLength="11" id="tc" className="form-control" placeholder="T.C. Kimlik Numarası" aria-describedby="basic-addon1" required
+                  onInvalid={e => e.target.setCustomValidity('Lütfen T.C. Kimlik Numaranızı doldurunuz.')} onInput={e => e.target.setCustomValidity('')} />
               </div>
               <div className="input-group mb-3 mt-4">
                 <div id="passwordp" className="input-group-prepend">
-                  <span className="input-group-text h-100" id="basic-addon2"><i className="fa fa-lock"></i></span>
+                  <span className="input-group-text h-100" id="basic-addon2" style={{background:"#D5AA41", borderColor:"#D5AA41"}}><i className="fa fa-lock"></i></span>
                 </div>
                 <input type="password" id="password" className="form-control" placeholder="Şifre" aria-describedby="basic-addon2" required
-                 onInvalid={e => e.target.setCustomValidity('Lütfen şifrenizi doldurunuz.')} onInput={e => e.target.setCustomValidity('')}/>
+                  onInvalid={e => e.target.setCustomValidity('Lütfen şifrenizi doldurunuz.')} onInput={e => e.target.setCustomValidity('')} />
               </div>
               <div className="mt-2 mb-3" style={{ overflow: "hidden" }}>
-                <button type="submit" className="btn btn-success mt-2" style={{ float: "left", width: "45%" }}>Giriş Yap</button>
-                <button type="button" className="btn btn-primary mt-2" style={{ float: "right", width: "45%" }}>Kayıt Ol</button>
+                <button type="submit" className="button mt-3 mb-3" style={{ marginLeft:"12px",float: "left", width: "43%" }}><i className="fa fa-sign-in"></i>&nbsp;Giriş Yap</button>
+                <button type="submit" className="button mt-3 mb-3" style={{ marginRight:"12px",float: "right", width: "43%" }}><i className="fa fa-plus-circle"></i>&nbsp;Kayıt Ol</button>
               </div>
-              <div className="forgot" style={{textAlign:"center"}}>
-                <a className="forgot" href="#password"><i className="fa fa-key forgot2"> &nbsp;</i>Şifremi Unuttum</a>
+              <div className="forgot" style={{ textAlign: "center" }}>
+                <a className="forgot" href="#"><i className="fa fa-key forgot2"> &nbsp;</i>Şifremi Unuttum</a>
               </div>
             </form>
           </div>
