5881c65d9393590d30d3d28a3be045c77c0814c6
Asım Kaymak <kaymakasm@gmail.com>
2022-04-21 04:10:58 +0300

    nexjs init

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+
+import { useEffect } from "react";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
