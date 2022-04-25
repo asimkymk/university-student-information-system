bdaad522f9e5fd0c9264afaaeafc7e33b055660d
emncnozge <emincanozge@outlook.com>
2022-04-25 05:03:22 +0300

    Update

---
+++ pages/login/index.js
@@ -0,0 +1,128 @@
+/*TODO
+REFACTOR BUTTON STYLE
+CUSTOMINPUTTEXT
+CLEAN CODE*/
+
+import "font-awesome/css/font-awesome.min.css";
+import loginStyles from "../../styles/Login.module.css";
+import CustomButton from "../../components/CustomButton";
+
+export default function Login() {
+  return (
+    <div className={loginStyles.body}>
+      <div className={`container ${loginStyles.containerText}`}>
+        <div className="row" style={{ minHeight: "100vh" }}>
+          <div
+            className="col-md-7 col-lg-5 col-10 mx-auto my-auto"
+            style={{ position: "relative", top: "-50px" }}
+          >
+            <img
+              className={loginStyles.loginLogo}
+              src="/login/iuclogo.png"
+              alt="Logo"
+            ></img>
+            <div className={loginStyles.loginArea}>
+              <form>
+                <div
+                  className={`input-group mb-3 mt-5 ${loginStyles.inputGroup}`}
+                >
+                  <div id="tcp" className="input-group-prepend">
+                    <span
+                      className={`input-group-text h-100 ${loginStyles.inputGroupText}`}
+                      id="basic-addon1"
+                      style={{ background: "#D5AA41", borderColor: "#D5AA41" }}
+                    >
+                      <i className="fa fa-id-badge "></i>
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    maxLength="11"
+                    id="tc"
+                    className={`form-control ${loginStyles.formControl}`}
+                    placeholder="T.C. Kimlik Numarası"
+                    aria-describedby="basic-addon1"
+                    required
+                    onInvalid={(e) =>
+                      e.target.setCustomValidity(
+                        "Lütfen T.C. Kimlik Numaranızı doldurunuz."
+                      )
+                    }
+                    onInput={(e) => e.target.setCustomValidity("")}
+                  />
+                </div>
+                <div
+                  className={`input-group mb-3 mt-4 ${loginStyles.inputGroup}`}
+                >
+                  <div id="passwordp" className="input-group-prepend">
+                    <span
+                      className={`input-group-text h-100 ${loginStyles.inputGroupText}`}
+                      id="basic-addon2"
+                      style={{ background: "#D5AA41", borderColor: "#D5AA41" }}
+                    >
+                      <i className="fa fa-lock"></i>
+                    </span>
+                  </div>
+                  <input
+                    type="password"
+                    id="password"
+                    className={`form-control ${loginStyles.formControl}`}
+                    placeholder="Şifre"
+                    aria-describedby="basic-addon2"
+                    required
+                    onInvalid={(e) =>
+                      e.target.setCustomValidity("Lütfen şifrenizi doldurunuz.")
+                    }
+                    onInput={(e) => e.target.setCustomValidity("")}
+                  />
+                </div>
+                <div style={{marginLeft: "10px"}}>
+                  <label className={`switch ${loginStyles.switch}`}>
+                    <input type="checkbox" />
+                    <span className={`slider round ${loginStyles.slider} ${loginStyles.round}`}></span>
+                  </label>
+                  <span style={{verticalAlign:"bottom", color:"#232323"}}>&nbsp;Beni Hatırla</span>
+                </div>
+                <div className="mt-2 mb-3" style={{ overflow: "hidden" }}>
+                  <CustomButton
+                    id="btn1"
+                    type="submit"
+                    className={`button mt-3 mb-3 ${loginStyles.button}`}
+                    style={{ marginLeft: "12px", float: "left", width: "43%" }}
+                    icon={`fa fa-sign-in ${loginStyles.faSignIn}`}
+                    value="Giriş Yap"
+                  ></CustomButton>
+
+                  <CustomButton
+                    id="btn2"
+                    type="submit"
+                    className={`button mt-3 mb-3 ${loginStyles.button}`}
+                    style={{
+                      marginRight: "12px",
+                      float: "right",
+                      width: "43%",
+                    }}
+                    icon={`fa fa-plus-circle ${loginStyles.faPlusCircle}`}
+                    value="Kayıt Ol"
+                  ></CustomButton>
+                </div>
+                <div
+                  className={loginStyles.forgot}
+                  style={{ textAlign: "center" }}
+                >
+                  <a className={loginStyles.forgot} style={{color:"#232323"}} href="#">
+                    <i className={`fa fa-key ${loginStyles.forgot2}`}>
+                      {" "}
+                      &nbsp;
+                    </i>
+                    Şifremi Unuttum
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,132 @@
+import "./bootstrap/css/bootstrap.min.css";
+import "./web.css";
+import img from "./iuclogo.png";
+import "font-awesome/css/font-awesome.min.css";
+import { Modal } from "react-bootstrap";
+import { useState } from "react";
+<script type="text/javascript" src="/bootstrap/js/bootstrap.min.js" />;
+
+function App() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div className="container">
+      <div className="row" style={{ minHeight: "100vh" }}>
+        <div
+          className="col-md-7 col-lg-4 col-10 mx-auto my-auto"
+          style={{ position: "relative", top: "-50px" }}
+        >
+          <img className="login-logo" src={img} alt="Logo"></img>
+          <div className="login-area">
+            <form>
+              <div className="input-group mb-3 mt-5">
+                <div id="tcp" className="input-group-prepend">
+                  <span
+                    className="input-group-text h-100"
+                    id="basic-addon1"
+                    style={{ background: "#D5AA41", borderColor: "#D5AA41" }}
+                  >
+                    <i className="fa fa-id-badge "></i>
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  maxLength="11"
+                  id="tc"
+                  className="form-control"
+                  placeholder="T.C. Kimlik Numarası"
+                  aria-describedby="basic-addon1"
+                  required
+                />
+              </div>
+              <div className="input-group mb-3 mt-4">
+                <div id="passwordp" className="input-group-prepend">
+                  <span
+                    className="input-group-text h-100"
+                    id="basic-addon2"
+                    style={{ background: "#D5AA41", borderColor: "#D5AA41" }}
+                  >
+                    <i className="fa fa-lock"></i>
+                  </span>
+                </div>
+                <input
+                  type="password"
+                  id="password"
+                  minLength="8"
+                  className="form-control"
+                  placeholder="Şifre"
+                  aria-describedby="basic-addon2"
+                  required
+                />
+              </div>
+              <div className="mt-2 mb-3" style={{ overflow: "hidden" }}>
+                <button
+                  type="submit"
+                  className="button mt-3 mb-3"
+                  style={{ marginLeft: "12px", float: "left", width: "43%" }}
+                >
+                  <i className="fa fa-sign-in"></i>&nbsp;Giriş Yap
+                </button>
+                <button
+                  type="submit"
+                  className="button mt-3 mb-3"
+                  style={{ marginRight: "12px", float: "right", width: "43%" }}
+                >
+                  <i className="fa fa-plus-circle"></i>&nbsp;Kayıt Ol
+                </button>
+              </div>
+              <div className="forgot" style={{ textAlign: "center" }}>
+                <a href="##" onClick={handleShow} className="forgot">
+                  <i className="fa fa-key forgot2"> &nbsp;</i>Şifremi Unuttum
+                </a>
+              </div>
+
+              <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                  <Modal.Title>Şifremi Unuttum</Modal.Title>
+                </Modal.Header>
+                <form>
+                  <Modal.Body>
+                    <div className="input-group">
+                      <div id="tcp" className="input-group-prepend">
+                        <span
+                          className="input-group-text h-100"
+                          id="basic-addon1"
+                          style={{
+                            background: "#D5AA41",
+                            borderColor: "#D5AA41",
+                          }}
+                        >
+                          <i className="fa fa-id-badge"></i>
+                        </span>
+                      </div>
+                      <input
+                        type="text"
+                        minLength="11"
+                        maxLength="11"
+                        id="tc"
+                        className="form-control"
+                        placeholder="T.C. Kimlik Numarası"
+                        aria-describedby="basic-addon1"
+                        required
+                      />
+                    </div>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <button type="submit" className="button w-100">
+                      <i className="fa fa-paper-plane"></i>&nbsp;Gönder
+                    </button>
+                  </Modal.Footer>
+                </form>
+              </Modal>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
