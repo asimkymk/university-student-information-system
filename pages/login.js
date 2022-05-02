db3bab10bedffe3a4bdf90eedb348b62e889ea64
Emin Can ÖZGE <emincanozge@outlook.com>
2022-05-02 16:54:48 +0300

    Register modal is edited.

--- pages/login.js
+++ pages/login.js
@@ -17,9 +17,8 @@ import {
   faCalendar,
   faMarsAndVenus,
   faFont,
+  faAt,
 } from "@fortawesome/free-solid-svg-icons";
-
-import Button from "@mui/material/Button";
 export default function Login() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -27,16 +26,17 @@ export default function Login() {
   const [show2, setShow2] = useState(false);
   const handleClose2 = () => setShow2(false);
   const handleShow2 = () => setShow2(true);
-  const idBadgeElement = <FontAwesomeIcon icon={faIdBadge} className={`mx-auto faIdBadge`} />;
-  const lockElement = <FontAwesomeIcon icon={faLock} className={`mx-auto faLock`} />;
+  const idBadgeElement = (
+    <FontAwesomeIcon icon={faIdBadge} className={`mx-auto faIdBadge`} />
+  );
+  const lockElement = (
+    <FontAwesomeIcon icon={faLock} className={`mx-auto faLock`} />
+  );
   const signElement = (
-    <FontAwesomeIcon icon={faSignIn} className={`mx-auto faSignIn`}
-    />
+    <FontAwesomeIcon icon={faSignIn} className={`mx-auto faSignIn`} />
   );
   const registerElement = (
-    <FontAwesomeIcon icon={faPlusCircle} className={`faPlusCircle mx-auto`}
-    />
-
+    <FontAwesomeIcon icon={faPlusCircle} className={`faPlusCircle mx-auto`} />
   );
   const forgotPasswordElement = (
     <FontAwesomeIcon
@@ -47,47 +47,35 @@ export default function Login() {
     </FontAwesomeIcon>
   );
   const nameSurnameElement = (
-    <FontAwesomeIcon
-      icon={faFont}
-      className={`mx-auto`}
-
-    >
+    <FontAwesomeIcon icon={faFont} className={`mx-auto`}>
+      &nbsp;
+    </FontAwesomeIcon>
+  );
+  const mailElement = (
+    <FontAwesomeIcon icon={faAt} className={`mx-auto`}>
       &nbsp;
     </FontAwesomeIcon>
   );
   const bdayElement = (
-    <FontAwesomeIcon
-      icon={faCalendar}
-      className={`mx-auto`}
-
-    >
+    <FontAwesomeIcon icon={faCalendar} className={`mx-auto`}>
       &nbsp;
     </FontAwesomeIcon>
   );
   const passElement = (
-    <FontAwesomeIcon
-      icon={faLock}
-      className={`mx-auto`}
-
-    >
+    <FontAwesomeIcon icon={faLock} className={`mx-auto`}>
       &nbsp;
     </FontAwesomeIcon>
   );
   const genderElement = (
-    <FontAwesomeIcon
-      icon={faMarsAndVenus}
-      className={`mx-auto`}
-
-    >
+    <FontAwesomeIcon icon={faMarsAndVenus} className={`mx-auto`}>
       &nbsp;
     </FontAwesomeIcon>
   );
 
   const sendElement = (
-    <FontAwesomeIcon icon={faPaperPlane}
-      className={`faPaperPlane mx-auto`}
-    />
+    <FontAwesomeIcon icon={faPaperPlane} className={`faPaperPlane mx-auto`} />
   );
+
   function createDate() {
     var date = new Date();
     date.setDate(31);
@@ -113,7 +101,10 @@ export default function Login() {
                 <div
                   className={`input-group mb-3 mt-5 ${loginStyles.inputGroup}`}
                 >
-                  <div id="tcp" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
+                  <div
+                    id="tcp"
+                    className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
+                  >
                     <span
                       className={`input-group-text h-100 ${loginStyles.inputGroupText}`}
                       id="basic-addon1"
@@ -141,7 +132,10 @@ export default function Login() {
                 <div
                   className={`input-group mb-3 mt-4 ${loginStyles.inputGroup}`}
                 >
-                  <div id="passwordp" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
+                  <div
+                    id="passwordp"
+                    className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
+                  >
                     <span
                       className={`input-group-text h-100 ${loginStyles.inputGroupText}`}
                       id="basic-addon2"
@@ -214,14 +208,17 @@ export default function Login() {
                     &nbsp;Şifremi Unuttum
                   </a>
                 </div>
-                <Modal show={show} onHide={handleClose} centered>
+                <Modal show={show} onHide={handleClose} centered className={`mx-auto my-auto`}>
                   <Modal.Header closeButton>
                     <Modal.Title>Şifremi Unuttum</Modal.Title>
                   </Modal.Header>
                   <form>
                     <Modal.Body>
                       <div className={`input-group ${loginStyles.inputGroup}`}>
-                        <div id="tcp" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
+                        <div
+                          id="tcp"
+                          className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
+                        >
                           <span
                             className={`input-group-text ${loginStyles.inputGroupText} h-100`}
                             id="basic-addon1"
@@ -268,8 +265,14 @@ export default function Login() {
                   </Modal.Header>
                   <form>
                     <Modal.Body>
-                      <div className={`input-group mb-3 ${loginStyles.inputGroup}`}>
-                        <div id="tcp" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
+                      <div className={`${loginStyles.label}`}>T.C. Kimlik Numarası</div>
+                      <div
+                        className={`input-group mb-2 ${loginStyles.inputGroup}`}
+                      >
+                        <div
+                          id="tcp"
+                          className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
+                        >
                           <span
                             className={`input-group-text ${loginStyles.inputGroupText} h-100`}
                             id="basic-addon1"
@@ -292,9 +295,74 @@ export default function Login() {
                           required
                         />
                       </div>
-
-                      <div className={`input-group mb-3 ${loginStyles.inputGroup}`}>
-                        <div id="adp" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
+                      <div style={{ width: "49%", float: "left" }}>
+                        <div className={`${loginStyles.label}`}>Ad</div>
+                        <div
+                          className={`input-group mb-2 ${loginStyles.inputGroup}`}
+                        >
+                          <div
+                            id="adp"
+                            className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
+                          >
+                            <span
+                              className={`input-group-text ${loginStyles.inputGroupText} h-100`}
+                              id="basic-addon1"
+                              style={{
+                                background: "#D5AA41",
+                                borderColor: "#D5AA41",
+                              }}
+                            >
+                              {nameSurnameElement}
+                            </span>
+                          </div>
+                          <input
+                            type="text"
+                            id="ad"
+                            className={`form-control ${loginStyles.formControl}`}
+                            placeholder="Ad"
+                            aria-describedby="basic-addon1"
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div style={{ float: "right", width: "49%" }}>
+                        <div className={`${loginStyles.label}`}>Soyad</div>
+                        <div
+                          className={`input-group mb-2 ${loginStyles.inputGroup}`}
+                        >
+                          <div
+                            id="soyadp"
+                            className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
+                          >
+                            <span
+                              className={`input-group-text ${loginStyles.inputGroupText} h-100`}
+                              id="basic-addon1"
+                              style={{
+                                background: "#D5AA41",
+                                borderColor: "#D5AA41",
+                              }}
+                            >
+                              {nameSurnameElement}
+                            </span>
+                          </div>
+                          <input
+                            type="text"
+                            id="soyad"
+                            className={`form-control ${loginStyles.formControl}`}
+                            placeholder="Soyad"
+                            aria-describedby="basic-addon1"
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div className={`${loginStyles.label}`}>E-Posta Adresi</div>
+                      <div
+                        className={`input-group mb-2 ${loginStyles.inputGroup}`}
+                      >
+                        <div
+                          id="eposta"
+                          className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
+                        >
                           <span
                             className={`input-group-text ${loginStyles.inputGroupText} h-100`}
                             id="basic-addon1"
@@ -303,87 +371,89 @@ export default function Login() {
                               borderColor: "#D5AA41",
                             }}
                           >
-                            {nameSurnameElement}
+                            {mailElement}
                           </span>
                         </div>
                         <input
-                          type="text"
-                          id="ad"
+                          type="password"
+                          id="mail"
                           className={`form-control ${loginStyles.formControl}`}
-                          placeholder="Ad"
+                          placeholder="E-Posta"
                           aria-describedby="basic-addon1"
                           required
                         />
                       </div>
-                      <div className={`input-group mb-3 ${loginStyles.inputGroup}`}>
-                        <div id="soyadp" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
-                          <span
-                            className={`input-group-text ${loginStyles.inputGroupText} h-100`}
-                            id="basic-addon1"
-                            style={{
-                              background: "#D5AA41",
-                              borderColor: "#D5AA41",
-                            }}
+                      <div style={{ width: "49%", float: "left" }}>
+                        <div className={`${loginStyles.label}`}>Doğum Tarihi</div>
+                        <div
+                          className={`input-group mb-2 ${loginStyles.inputGroup}`}
+                        >
+                          <div
+                            id="dgp"
+                            className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
                           >
-                            {nameSurnameElement}
-                          </span>
+                            <span
+                              className={`input-group-text ${loginStyles.inputGroupText} h-100`}
+                              id="basic-addon1"
+                              style={{
+                                background: "#D5AA41",
+                                borderColor: "#D5AA41",
+                              }}
+                            >
+                              {bdayElement}
+                            </span>
+                          </div>
+                          <input
+                            type="date"
+                            max={createDate().toISOString().split("T")[0]}
+                            id="dt"
+                            className={`form-control ${loginStyles.formControl}`}
+                            placeholder="Doğum Tarihi"
+                            aria-describedby="basic-addon1"
+                            required
+                          />
                         </div>
-                        <input
-                          type="text"
-                          id="soyad"
-                          className={`form-control ${loginStyles.formControl}`}
-                          placeholder="Soyad"
-                          aria-describedby="basic-addon1"
-                          required
-                        />
                       </div>
-
-                      <div className={`input-group mb-3 ${loginStyles.inputGroup}`}>
-                        <div id="dgp" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
-                          <span
-                            className={`input-group-text ${loginStyles.inputGroupText} h-100`}
-                            id="basic-addon1"
-                            style={{
-                              background: "#D5AA41",
-                              borderColor: "#D5AA41",
-                            }}
+                      <div style={{ width: "49%", float: "right" }}>
+                        <div className={`${loginStyles.label}`}>Cinsiyet</div>
+                        <div
+                          className={`input-group mb-2 ${loginStyles.inputGroup}`}
+                        >
+                          <div
+                            id="cinsiyetp"
+                            className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
                           >
-                            {bdayElement}
-                          </span>
-                        </div>
-                        <input
-                          type="date"
-                          max={createDate().toISOString().split('T')[0]}
-                          id="dt"
-                          className={`form-control ${loginStyles.formControl}`}
-                          placeholder="Doğum Tarihi"
-                          aria-describedby="basic-addon1"
-                          required
-                        />
-                      </div>
-                      <div className={`input-group mb-3 ${loginStyles.inputGroup}`}>
-                        <div id="cinsiyetp" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
-                          <span
-                            className={`input-group-text ${loginStyles.inputGroupText} h-100`}
-                            id="basic-addon1"
-                            style={{
-                              background: "#D5AA41",
-                              borderColor: "#D5AA41",
-                            }}
+                            <span
+                              className={`input-group-text ${loginStyles.inputGroupText} h-100`}
+                              id="basic-addon1"
+                              style={{
+                                background: "#D5AA41",
+                                borderColor: "#D5AA41",
+                              }}
+                            >
+                              {genderElement}
+                            </span>
+                          </div>
+                          <select
+                            id="cinsiyet"
+                            className={`form-select select ${loginStyles.formControl} ${loginStyles.select}`}
+                            required
                           >
-                            {genderElement}
-                          </span>
+                            <option value="" disabled selected>Cinsiyet seçiniz</option>
+                            <option value="m">Erkek</option>
+                            <option value="f">Kadın</option>
+                            <option value="ng">Belirtmek istemiyorum</option>
+                          </select>
                         </div>
-                        <select id="cinsiyet" class="form-select" required>
-                          <option selected>Cinsiyet</option>
-                          <option value="m">Erkek</option>
-                          <option value="f">Kadın</option>
-                          <option value="ng">Belirtmek istemiyorum</option>
-                        </select>
                       </div>
-
-                      <div className={`input-group mb-3 ${loginStyles.inputGroup}`}>
-                        <div id="sifre" className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}>
+                      <div className={`${loginStyles.label}`}>Şifre</div>
+                      <div
+                        className={`input-group mb-2 ${loginStyles.inputGroup}`}
+                      >
+                        <div
+                          id="sifrep"
+                          className={`input-group-prepend my-auto mx-auto ${loginStyles.inputGroupPrepend}`}
+                        >
                           <span
                             className={`input-group-text ${loginStyles.inputGroupText} h-100`}
                             id="basic-addon1"
@@ -397,17 +467,13 @@ export default function Login() {
                         </div>
                         <input
                           type="password"
-                          minLength="11"
-                          maxLength="11"
-                          id="tcf2"
+                          id="sifre"
                           className={`form-control ${loginStyles.formControl}`}
                           placeholder="Şifre"
                           aria-describedby="basic-addon1"
                           required
                         />
                       </div>
-
-
                     </Modal.Body>
                     <Modal.Footer>
                       <button
@@ -420,8 +486,8 @@ export default function Login() {
                           width: "43%",
                         }}
                       >
-                        {sendElement}
-                        &nbsp; Gönder
+                        {registerElement}
+                        &nbsp; Kayıt Ol
                       </button>
                     </Modal.Footer>
                   </form>
