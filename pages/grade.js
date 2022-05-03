bb39eeb8ade45729b14fdc224b2843bb50457806
Asım Kaymak <kaymakasm@gmail.com>
2022-05-03 17:42:35 +0300

    notlar ufak uğraşı cnaım sıkıldı

--- pages/grade.js
+++ pages/grade.js
@@ -23,22 +23,52 @@ class Home extends Component {
           <title>Not Bilgisi - İÜC ÖBS</title>
         </Head>
         <Layout
-          appBar={{ links: { "Ana Sayfa": "#", Notlar: "#" }, title: "Notlar" }}
+          appBar={{
+            links: { "Ana Sayfa": "#", "Not Bilgisi": "#" },
+            title: "Not Bilgisi",
+          }}
         >
           <div className={styles.gradePanel}>
-            <table>
+            <div className={styles.gradePanelHeader}>
+              <h6 className={styles.gradePanelHeaderText}>Dönem Notları</h6>
+            </div>
+            <table className="table align-items-center mb-0 mt-2">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>DERS ADI</th>
-                  <th>HARF NOTU</th>
+                  <th
+                    className={`text-uppercase text-secondary opacity-7 ${styles.textxxs} ${styles.fontWeightBolder}`}
+                  >
+                    Ders Adı
+                  </th>
+                  <th
+                    className={`text-uppercase text-secondary opacity-7 ${styles.textxxs} ${styles.fontWeightBolder}`}
+                  >
+                    Öğretİm GörevlİSİ
+                  </th>
+                  <th
+                    className={`text-uppercase text-secondary opacity-7 ${styles.textxxs} ${styles.fontWeightBolder}`}
+                  >
+                    Harf Notu
+                  </th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>1</td>
-                  <td>BİLGİSAYAR MİMARİSİ</td>
-                  <td>AA</td>
+                  <td>
+                    <h6 class="mb-0 text-sm">Bilgisayar Mimarisi</h6>
+                  </td>
+                  <td>
+                    <p class="text-xs text-secondary mb-0">
+                      Prof. Dr. Ahmet Sertbaş
+                    </p>
+                  </td>
+                  <td>
+                    <span
+                      className={`text-secondary ${styles.textxs} ${styles.fontWeightBold}`}
+                    >
+                      AA
+                    </span>
+                  </td>
                 </tr>
               </tbody>
             </table>
