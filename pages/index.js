24a5102fc22895926f7cd03ac7c4597ef1f140ba
Emin Can ÖZGE <emincanozge@outlook.com>
2022-05-06 03:23:47 +0300

    GPA chart added.

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,15 @@ import React, { Component } from "react";
 import styles from "../styles/Home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Layout from "../components/Layout/Layout";
+import Chart from "../components/Chart/Chart";
+
 import {
   faBook,
   faStar,
-  faCalendar
+  faCalendar,
+  faPencil,
+  faSuitcase,
+  faX,
 } from "@fortawesome/free-solid-svg-icons";
 const bookElement = (
   <FontAwesomeIcon icon={faBook} />
@@ -17,6 +22,16 @@ const starElement = (
 const classElement = (
   <FontAwesomeIcon icon={faCalendar} />
 );
+const crossElement = (
+  <FontAwesomeIcon icon={faX} />
+);
+const workElement = (
+  <FontAwesomeIcon icon={faSuitcase} />
+);
+const homeworkElement = (
+  <FontAwesomeIcon icon={faPencil} />
+);
+
 class Home extends Component {
   //TASRIM İÇİN GEÇİCİ ÇÖZÜM
   //NORMALDE AUTHENTICATION İLE ÇÖZÜM ÜRETİLECEK.
@@ -42,11 +57,14 @@ class Home extends Component {
           appBar={{ links: { "Ana Sayfa": "/" }, title: "Ana Sayfa" }}
         >
           <div className={`container ${styles.container}`}>
+
+
+
             <div className="row d-flex justify-content-between">
               <div className={`col col-xl-4 col-lg-6 col-md-6 col-sm-12`}>
                 <div className={`card ${styles.reddb} ${styles.card}`}>
-                  <div className="row h-100">
-                    <div className={`col-2 h-100 my-auto mx-auto`}>
+                  <div className="row ">
+                    <div className={`col-2  my-auto mx-auto`}>
                       <div className={`${styles.squareTop} ${styles.blue}`}>
                         <div className={`${styles.icons}`}>
                           {bookElement}
@@ -111,6 +129,89 @@ class Home extends Component {
                     <div className={`${styles.horizontalLine}`}></div>
                     <div className={`${styles.info}`}>&nbsp;</div>
                   </div>
+                </div>
+              </div>
+              <div className={`col col-xl-4 col-lg-6 col-md-6 col-sm-12`}>
+                <div className={`card ${styles.greendb} ${styles.card}`}>
+                  <div className="row">
+                    <div className={`col-2 my-auto mx-auto`}>
+                      <div className={`${styles.squareTop} ${styles.orange}`}>
+                        <div className={`${styles.icons}`}>
+                          {crossElement}
+                        </div>
+                      </div>
+                    </div>
+                    <div className={`col-10 mx-auto my-auto ${styles.cardSag}`}>
+                      <div className={`card-body`}>
+                        <h5 className={`card-title ${styles.cardTitle}`}>Kalınan Ders Sayısı</h5>
+                        <p className={`card-text ${styles.cardText}`}>2</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className={`${styles.horizontalLine}`}></div>
+                    <div className={`${styles.info}`}>&nbsp;</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className={`col col-xl-4 col-lg-6 col-md-6 col-sm-12`}>
+                <div className={`card ${styles.greendb} ${styles.card}`}>
+                  <div className="row">
+                    <div className={`col-2 my-auto mx-auto`}>
+                      <div className={`${styles.squareTop} ${styles.purple}`}>
+                        <div className={`${styles.icons}`}>
+                          {workElement}
+                        </div>
+                      </div>
+                    </div>
+                    <div className={`col-10 mx-auto my-auto ${styles.cardSag}`}>
+                      <div className={`card-body`}>
+                        <h5 className={`card-title ${styles.cardTitle}`}>Yapılan Staj Günü</h5>
+                        <p className={`card-text ${styles.cardText}`}>50</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className={`${styles.horizontalLine}`}></div>
+                    <div className={`${styles.info}`}>&nbsp;</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className={`col col-xl-4 col-lg-6 col-md-6 col-sm-12`}>
+                <div className={`card ${styles.greendb} ${styles.card}`}>
+                  <div className="row">
+                    <div className={`col-2 my-auto mx-auto`}>
+                      <div className={`${styles.squareTop} ${styles.yellow}`}>
+                        <div className={`${styles.icons}`}>
+                          {homeworkElement}
+                        </div>
+                      </div>
+                    </div>
+                    <div className={`col-10 mx-auto my-auto ${styles.cardSag}`}>
+                      <div className={`card-body`}>
+                        <h5 className={`card-title ${styles.cardTitle}`}>Bekleyen Ödev Sayısı</h5>
+                        <p className={`card-text ${styles.cardText}`}>1</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className={`${styles.horizontalLine}`}></div>
+                    <div className={`${styles.info}`}>&nbsp;</div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div className={`row`} style={{ margin: "0px", marginTop: "0" }}>
+              <div className={`card mt-3 ${styles.card}`}>
+                <div className="row">
+                  <span style={{ textAlign: "center", fontSize: "14pt", fontWeight: "lighter" }}>Dönem Ortalamaları</span>
+                  <div className={`${styles.horizontalLine}`}></div>
+                </div>
+                <div className="row mt-0">
+                  <div className={`${styles.chart}`}><Chart id="mychart"></Chart></div>
                 </div>
               </div>
             </div>
