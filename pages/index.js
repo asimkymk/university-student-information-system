d005afb8a7434a4fd9f731160d8b3ebde7b56d62
Emin Can ÖZGE <emincanozge@outlook.com>
2022-05-07 20:24:45 +0300

    Revert "Carousel added"
    This reverts commit 554050c4c041c52e3e8a8e043cc2fac08095175b.

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Layout from "../components/Layout/Layout";
 import Chart from "../components/Chart/Chart";
 import Chart2 from "../components/Chart/Chart2";
+
+
 import {
   faBook,
   faStar,
@@ -12,6 +14,7 @@ import {
   faPencil,
   faSuitcase,
   faX,
+  faChartBar,
   faChartColumn,
   faHashtag,
 } from "@fortawesome/free-solid-svg-icons";
@@ -60,47 +63,14 @@ class Home extends Component {
       <>
         <Head>
           <title>Ana Sayfa - İÜC ÖBS</title>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-          <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
         </Head>
         <Layout
           appBar={{ links: { "Ana Sayfa": "/" }, title: "Ana Sayfa" }}
         >
           <div className={`container ${styles.container}`}>
-            <div className="row my-auto m-0 mb-4" >
-              <div className={`card my-auto d-block pb-2 w-100 ${styles.card}`} style={{ height: "50vh" }}>
-                <div id="carouselIndicators" className="carousel slide" data-ride="carousel">
 
-                  <div className="carousel-inner">
-                    <div className="carousel-item active">
-                      <img className="d-block w-100" src="https://cdn.istanbulc.edu.tr/FileHandler.ashx?f=52KKRk-4JUCgfDWU-q3etA&r=huawei-ict-yarismasi-turkiye-2021-2022-5100350032006B004200350036006500700057004C0039007900700046006200610041004F003200370077003200" alt="First slide" />
-                      </div>
-                    <div className="carousel-item">
-                      <img className="d-block w-100" src="https://cdn.istanbulc.edu.tr/FileHandler.ashx?f=680f27EBZUOSDkWzYr6MJw&r=low-code-gelistirme-platformu-appcube-webinar-serisi-16-23-30-mart-1500-1600-65005A004D004C007700300074004B00330071007A0039007900700046006200610041004F003200370077003200" alt="Second slide" />
-                        </div>
-                    <div className="carousel-item">
-                      <img className="d-block w-100" src="https://cdn.istanbulc.edu.tr/FileHandler.ashx?f=-gIBd7IGCESJZkWgT6qkow&r=iuc-bilgisayar-kulubu-back-to-the-future-2021-etkinligi-4A007100760043005600660070006E0037003500480039007900700046006200610041004F003200370077003200" alt="Third slide" />
-                      </div>
-                  </div>
-                  <i className="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                  </i>
-                  <i className="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                  </i>
-                  <ol className="carousel-indicators">
-                    <li data-target="#carouselIndicators" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="2"></li>
-                  </ol>
-                </div>
-              </div>
-            </div>
+
+
             <div className="row d-flex justify-content-between">
               <div className={`col col-xl-4 col-lg-6 col-md-6 col-sm-12`}>
                 <div className={`card ${styles.reddb} ${styles.card}`}>
@@ -251,7 +221,7 @@ class Home extends Component {
                 <div className={`card ${styles.card} ${styles.chartCard}`}>
                   <div className="row">
 
-                    <span className={`${styles.chartText}`}>Fakültelerdeki Öğretim Görevlisi Sayıları</span>
+                    <span className={`${styles.chartText}`}>Dönem Ortalamaları</span>
                     <div>
                       <div className={`${styles.squareTopChart} ${styles.gray}`}>
                         <div className={`${styles.icons}`}>
@@ -270,7 +240,7 @@ class Home extends Component {
                   <div className="row">
 
                     <div className={styles.chartText}>
-                      Fakültelerdeki Öğrenci Sayıları
+                      Günlük Ders Sayısı
                     </div>
                     <div>
                       <div className={`${styles.squareTopChart} ${styles.brown}`}>
