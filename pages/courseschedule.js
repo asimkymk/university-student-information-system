d0881d24b3bbfe20f02af0f46783f751b75c4996
Emin Can ÖZGE <emincanozge@outlook.com>
2022-05-09 19:52:04 +0300

    Locale edits

--- pages/courseschedule.js
+++ pages/courseschedule.js
@@ -1,14 +1,13 @@
 import Head from "next/head";
 import React, { Component } from "react";
 import Layout from "../components/Layout/Layout";
-import Styles from "../styles/Syllabus.module.css";
 import { styled, alpha } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
     WeekView,
     Appointments,
+    AppointmentTooltip,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 import appointments from '../modules/today-appointments';
@@ -21,7 +20,7 @@ const Item = styled(Paper)(({ theme }) => ({
     overflowWrap: "break-word",
     backgroundColor: "rgb(255, 255, 255)",
     backgroundClip: "border-box",
-    border: "0px solid rgba(0, 0, 0, 0.125)",
+    border: "none",
     borderRadius: "0.75rem",
     boxShadow:
         "rgba(0, 0, 0, 0.1) 0rem 0.25rem 0.375rem -0.0625rem, rgba(0, 0, 0, 0.06) 0rem 0.125rem 0.25rem -0.0625rem",
@@ -98,21 +97,28 @@ class Home extends Component {
                     <title>Ders Programı - İÜC ÖBS</title>
                 </Head>
                 <Layout appBar={{ links: { "Ana Sayfa": "/", "Ders Programı": "/courseschedule" }, title: "Ders Programı" }}>
-                    <Item>
-                        <Scheduler
-                            data={appointments}
-                            height={660}
-                        >
-                            <ViewState />
-                            <WeekView
-                                startDayHour={8}
-                                endDayHour={19}
-                                timeTableCellComponent={TimeTableCell}
-                                dayScaleCellComponent={DayScaleCell}
-                            />
-                            <Appointments />
-                        </Scheduler>
-                    </Item>
+                    <div className="container">
+                        <Item>
+                            <Scheduler
+                                data={appointments}
+                                locale={"tr"}
+                                height={"100%"}
+                                minWidth={1}
+                            >
+                                <WeekView
+                                    excludedDays={[0, 6]}
+                                    startDayHour={7.68}
+                                    endDayHour={20.20}
+                                    cellDuration={50}
+
+                                />
+                                <Appointments />
+                                <AppointmentTooltip
+                                    showCloseButton
+                                />
+                            </Scheduler>
+                        </Item>
+                    </div>
                 </Layout>
             </>
         );
