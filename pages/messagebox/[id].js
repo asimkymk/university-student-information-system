b6c62259d6917f3925e8450c75c0831535cc3cba
Emin Can ÖZGE <emincanozge@outlook.com>
2022-05-15 01:23:27 +0300

    Message Box fix

--- pages/messagebox/[id].js
+++ pages/messagebox/[id].js
@@ -2,7 +2,6 @@ import styles from "../../styles/RequestAndObjection.module.css";
 import React from "react";
 import Layout from "../../components/Layout/Layout";
 import Head from "next/head";
-import Image from "next/image";
 import SendIcon from "@mui/icons-material/Send";
 import { useRouter } from "next/router";
 import Link from "next/link";
@@ -21,39 +20,15 @@ export default function Message() {
         }}
       >
         <div
-          className={`container ${styles.mainContainer} ${styles.mesajkutusu}`}
+          className={`container ${styles.mainContainer}`}
+          style={{ overflow: "hidden" }}
         >
-          <h5>Özgür Can Turna ile Mesajların</h5>
+          <h5 style={{ paddingLeft: "1rem", paddingTop: "1rem" }}>Özgür Can Turna ile mesajların</h5>
           <div className={`${styles.horizontalLine}`}></div>
-          <div className={styles.mesajlar}>
+
+          <div className="m-0" style={{ height: "50vh", overflowX: "hidden", overflowY: "auto", padding: "2rem", paddingTop: "0" }}>
             <div className="row">
               <div className="message-blue">
-                <div className="row">
-                  <p className="message-content">
-                    This is an awesomsdfsdfsdfd Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit. Reprehenderit ut quaerat
-                    veritatis perferendis? Aperiam veniam iure quae eaque, sed
-                    omnis eos assumenda minus excepturi veritatis porro repellat
-                    debitis dicta reprehenderit dignissimos cumque adipisci
-                    consequatur ipsam. Enim voluptas magni culpa. Cum, minima!
-                    Laboriosam inventore ipsam eum nemo fugiat suscipit,
-                    perspiciatis iure. e message! Lorem ipsum dolor sit amet
-                    consectetur adipisicing elit. Doloremque totam quam laborum
-                    fugit corporis tempore rem sed culpa provident eius. Labore
-                    debitis aspernatur vero tempore. Aperiam sit voluptate iusto
-                    magni velit doloremque vel? Obcaecati non nobis perferendis
-                    rem quaerat dolor, maiores provident repellat veritatis,
-                    ipsum quisquam omnis, unde eius ipsam.
-                  </p>
-                </div>
-
-                <div className="row">
-                  <div className="message-timestamp-left">SMS 13:37</div>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="message-orange">
                 <div className="row">
                   <p className="message-content">
                     This is an awesomsdfsdfsdfd Lorem ipsum dolor sit amet,
@@ -79,6 +54,32 @@ export default function Message() {
               </div>
             </div>
             <div className="row">
+              <div className="message-orange">
+                <div className="row">
+                  <p className="message-content">
+                    This is an awesomsdfsdfsdfd Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Reprehenderit ut quaerat
+                    veritatis perferendis? Aperiam veniam iure quae eaque, sed
+                    omnis eos assumenda minus excepturi veritatis porro repellat
+                    debitis dicta reprehenderit dignissimos cumque adipisci
+                    consequatur ipsam. Enim voluptas magni culpa. Cum, minima!
+                    Laboriosam inventore ipsam eum nemo fugiat suscipit,
+                    perspiciatis iure. e message! Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Doloremque totam quam laborum
+                    fugit corporis tempore rem sed culpa provident eius. Labore
+                    debitis aspernatur vero tempore. Aperiam sit voluptate iusto
+                    magni velit doloremque vel? Obcaecati non nobis perferendis
+                    rem quaerat dolor, maiores provident repellat veritatis,
+                    ipsum quisquam omnis, unde eius ipsam.
+                  </p>
+                </div>
+
+                <div className="row">
+                  <div className="message-timestamp-left">SMS 13:37</div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
               <div className="message-blue">
                 <div className="row">
                   <p className="message-content">
@@ -88,25 +89,26 @@ export default function Message() {
                 </div>
 
                 <div className="row">
-                  <div className="message-timestamp-left">SMS 13:37</div>
+                  <div className="message-timestamp-right">SMS 13:37</div>
                 </div>
               </div>
             </div>
           </div>
 
           <div className={`${styles.horizontalLine}`}></div>
-          <div className="row">
-            <div className="col-11">
+          <div className="row p-4 pt-0 pb-2">
+            <div className="col p-0">
               <div className="input-group">
                 <div className="input-group-prepend"></div>
                 <textarea
                   className="form-control"
                   aria-label="With textarea"
                   placeholder="Mesajınızı yazın"
+                  style={{ borderRadius: ".5rem" }}
                 ></textarea>
               </div>
             </div>
-            <div className={`col-1 mr-auto`}>
+            <div className={`p-0 mr-2`} style={{ width: "50px" }}>
               <Link href="/messagebox/1">
                 <a
                   type="button"
