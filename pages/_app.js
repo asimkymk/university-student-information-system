f307266a190d5b9b05cc923c011c439b7e9ca1cb
Emin Can ÖZGE <emincanozge@outlook.com>
2022-05-14 22:05:52 +0300

    Remove unnecessary package

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import '@syncfusion/ej2-base/styles/material.css';
-import '@syncfusion/ej2-buttons/styles/material.css';
-import '@syncfusion/ej2-calendars/styles/material.css';
-import '@syncfusion/ej2-dropdowns/styles/material.css';
-import '@syncfusion/ej2-inputs/styles/material.css';
-import '@syncfusion/ej2-navigations/styles/material.css';
-import '@syncfusion/ej2-popups/styles/material.css';
-import '@syncfusion/ej2-splitbuttons/styles/material.css';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
