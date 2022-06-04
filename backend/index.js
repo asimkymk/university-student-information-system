29f8fad170afdd7ff5a724cb2824ff590f27b86f
Emin Can ÖZGE <emincanozge@outlook.com>
2022-06-04 13:24:07 +0300

    Fix CORS

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+var cors = require("cors");
 const express = require("express"),
   app = express(),
   port = 3001;
@@ -8,6 +9,7 @@ const privateKey = "ASIM";
 var user;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
